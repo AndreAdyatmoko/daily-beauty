eb4a07ccd4645f617347a2219bbe9ade9243d142
AndreAdyatmoko <AndreAdyatmoko>
2024-01-22 20:32:30 +0700

    Add card

--- src/components/content/Content2.jsx
+++ src/components/content/Content2.jsx
@@ -6,23 +6,54 @@ const Content2 = () => {
   return (
     <div>
       <div className="border-t border-gray-500">
-        <div className="grid grid-cols-2 items-center font-serif text-gray-700">
+        <div className="grid grid-cols-4 gap-4 items-center font-serif text-gray-700">
           <div>
             <div className="flex gap-8 mb-28">
               <LeftArrow />
               <RightArrow />
             </div>
-            <h1 className="text-4xl font-bold">Dive into <br/> Beauty</h1>
+            <h1 className="text-4xl font-bold">
+              Dive into <br /> Beauty
+            </h1>
             <div className="mt-6">
               <Button2>See More</Button2>
             </div>
           </div>
-          <div>
+          <div className="border-solid border-2 px-2 border-gray-200 rounded-md">
             <img
               src="/assets/skin.jpg"
               alt="Skincare Products"
-              className="w-full h-auto object-cover"
+              className="w-full object-cover mt-2 rounded-sm"
+              style={{ height: "85%" }}
             />
+            <div className="flex items-center justify-between ">
+              <p className="text-black font-normal text-lg">Skincare</p>
+              <RightArrow />
+            </div>
+          </div>
+          <div className="border-solid border-2 px-2 border-gray-200 rounded-md">
+            <img
+              src="/assets/skin.jpg"
+              alt="Skincare Products"
+              className="w-full object-cover mt-2 rounded-sm"
+              style={{ height: "85%" }}
+            />
+            <div className="flex items-center justify-between ">
+              <p className="text-black font-normal text-lg">Haircare</p>
+              <RightArrow />
+            </div>
+          </div>
+          <div className="border-solid border-2 px-2 border-gray-200 rounded-md">
+            <img
+              src="/assets/skin.jpg"
+              alt="Skincare Products"
+              className="w-full object-cover mt-2 rounded-sm"
+              style={{ height: "85%" }}
+            />
+            <div className="flex items-center justify-between ">
+              <p className="text-black font-normal text-lg">Makeup</p>
+              <RightArrow />
+            </div>
           </div>
         </div>
       </div>
